425709d21d4f1a8bd6d73e3c9f3708798f432ac6
plafagency <34011868+plafagency@users.noreply.github.com>
2023-04-29 17:18:38 -0300

    Add logic for private content to Dashboard

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -2,44 +2,68 @@ import React from "react";
 import TopNavBar from "../nav/TopNavBar";
 import { Box, Container, Divider, Paper, Typography } from "@mui/material";
 import UserGreeting from "./UserGreeting";
+import { useAuth0 } from "@auth0/auth0-react";
+import Login from "../login/Login";
 
 function Home() {
+  const { user, isAuthenticated, isLoading } = useAuth0;
+
+  if (isLoading) {
+    return <div>Cargando...</div>;
+  }
+
+  if (!isAuthenticated) {
+    return (
+      <div>
+        <Login />
+      </div>
+    );
+  }
+
   return (
-    <div>
-      <TopNavBar />
-      <Container maxWidth="xl" sx={{ py: 3 }}>
-        <UserGreeting />
+    isAuthenticated && (
+      <div>
+        <TopNavBar />
+        <Container maxWidth="xl" sx={{ py: 3 }}>
+          <UserGreeting />
 
-        <Divider sx={{ pt: 3, mb: 3 }} />
-        <Box
-          sx={{
-            m: 3,
-            display: "flex",
-            flexDirection: { xs: "column", md: "row" },
-            gap: 3,
-            justifyContent: "space-between",
-          }}
-        >
-          <Paper elevation={4} sx={{ p: 3, width: { xs: "100%", md: "50%" } }}>
-            <Typography variant="h6">
-              ¿Qué puedes encontrar en el portal?
-            </Typography>
-            <Typography variant="paragraph">
-              En este portal encontrarás información importante sobre tu cuenta
-              de PLAF.
-            </Typography>
-          </Paper>
-          <Paper elevation={4} sx={{ p: 3, width: { xs: "100%", md: "50%" } }}>
-            <Typography variant="h6">¿Quiénes tienen acceso?</Typography>
+          <Divider sx={{ pt: 3, mb: 3 }} />
+          <Box
+            sx={{
+              m: 3,
+              display: "flex",
+              flexDirection: { xs: "column", md: "row" },
+              gap: 3,
+              justifyContent: "space-between",
+            }}
+          >
+            <Paper
+              elevation={4}
+              sx={{ p: 3, width: { xs: "100%", md: "50%" } }}
+            >
+              <Typography variant="h6">
+                ¿Qué puedes encontrar en el portal?
+              </Typography>
+              <Typography variant="paragraph">
+                En este portal encontrarás información importante sobre tu
+                cuenta de PLAF.
+              </Typography>
+            </Paper>
+            <Paper
+              elevation={4}
+              sx={{ p: 3, width: { xs: "100%", md: "50%" } }}
+            >
+              <Typography variant="h6">¿Quiénes tienen acceso?</Typography>
 
-            <Typography variant="paragraph">
-              Solo pueden acceder clientes de PLAF, y la información que se
-              encuentra disponible aquí es exclusiva para ti.
-            </Typography>
-          </Paper>
-        </Box>
-      </Container>
-    </div>
+              <Typography variant="paragraph">
+                Solo pueden acceder clientes de PLAF, y la información que se
+                encuentra disponible aquí es exclusiva para ti.
+              </Typography>
+            </Paper>
+          </Box>
+        </Container>
+      </div>
+    )
   );
 }
 
